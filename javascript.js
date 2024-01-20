cde364ee8daf0630480cb5c36fc9c05388a6d90c
michelle <michellegathonimain@gmail.com>
2024-01-20 18:52:36 +0300

    Create a function that gets the player's choice

--- javascript.js
+++ javascript.js
@@ -3,3 +3,9 @@ function getComputerChoice(){
     let computerChoice=choice[(Math.floor(Math.random()*choice.length))];
     return computerChoice;
 }
+function getPlayerChoice(){
+    let input=prompt('Choose one among "Rock", "Paper" or "Scissors"');
+    let choice=input.toLowerCase();
+    let playerChoice=choice[0].toUpperCase()+choice.slice(1);
+    return playerChoice;
+}
